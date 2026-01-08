33ae1cb63ea6401f1e566f64e5724de72c3ec10e
felipeaugusto1446 <felipeaugusto.2439@gmail.com>
2026-01-08 16:17:27 -0300

    criando as tabelas de usuario pedido e itens do pedido, e fazendo a migration da database

---
+++ models.py
@@ -0,0 +1,75 @@
+from sqlalchemy import create_engine, Column,String,Integer,Boolean,Float,ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils.types import ChoiceType
+#para iniciar o alembic rodar: python -m alembic init alembic
+#comando para fazer a migration: python -m alembic revision --autogenerate -m "Initial Migration"
+#comando para fazer o alembic executar o banco da ultima versão que eu foi gerada: python -m alembic upgrade head
+
+#cria a conexão com seu banco1
+db = create_engine("sqlite:///database/banco.db")
+
+
+#cria a base de banco de dados
+Base = declarative_base()
+    
+# classes/tabelas do banco
+# Usuario
+class Usuario(Base):
+    __tablename__ = "usuarios"
+    
+    id = Column("id",Integer,primary_key=True,autoincrement=True)
+    nome = Column("nome",String)
+    email = Column("email",String,nullable=False)
+    senha = Column("senha",String)
+    ativo = Column("ativo",Boolean)
+    admin = Column("admin",Boolean,default=False)
+    
+    
+    def __init__(self,nome,email,senha,ativo=True,admin=False):
+        self.nome = nome
+        self.emai = email
+        self.senha = senha
+        self.ativo = ativo
+        self.admin = admin
+    
+# Pedidos
+class Pedido(Base):
+    __tablename__ = "pedidos"
+    
+    # STATUS_PEDIDOS = (
+    #     ("PENDENTE","PENDENTE"),
+    #     ("CANCELADO","CANCELADO"),
+    #     ("FINALIZADO","FINALIZADO")
+    # )
+    
+    id = Column("id",Integer,primary_key=True,autoincrement=True)
+    status = Column("status",String) #pendente, cancelado,finalizado
+    usuario = Column("usuario",ForeignKey("usuarios.id"))
+    preco = Column("preco",Float)
+    # itens = 
+    
+    def __init__(self,usuario,status="PENDENTE",preco=0):
+        self.usuario =  usuario
+        self.preco = preco
+        self.status = status
+    
+    
+# ItensPedido
+class ItemPedido(Base):
+    __tablename__ = "itens_pedido"
+    
+    id = Column("id",Integer,primary_key=True,autoincrement=True)
+    quantidade = Column("quantidade",Integer)
+    sabor = Column("sabor",String)
+    tamanho = Column("tamanho",String)
+    preco_unitario = Column("preco_unitario",Float)
+    pedido = Column("pedido",ForeignKey("pedidos.id"))
+    
+    def __init__(self,quantidade,sabor,tamanho,preco_unitario,pedido):
+        self.quantidade = quantidade
+        self.sabor = sabor
+        self.tamanho =  tamanho
+        self.preco_unitario = preco_unitario
+        self.pedido = pedido
+
+#executa a criação dos metadados do seu banco(cria definitivamente o banco de dados)
